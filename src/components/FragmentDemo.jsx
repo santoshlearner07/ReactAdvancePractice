5af8aa71d6465c6aeb90a8c2dc8535a9e9ea9c48
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2022-03-09 23:24:04 +0530

    Santosh[Added];-React-Fragment

---
+++ src/components/FragmentDemo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FragmentDemo() {
+    return (
+        <React.Fragment>
+            <h1>FragmentDemo</h1>
+            <p>This is Fragment </p>
+        </React.Fragment>
+
+    )
+}
+
+export default FragmentDemo
